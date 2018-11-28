9829ace88cf67c8b49dee4418fc1852436498b09
Wurmatron <wurmatron@gmail.com>
2018-11-28 14:40:19 -0500

    Init GDX

---
+++ core/src/com/wurmcraft/towers/Towers.java
@@ -0,0 +1,14 @@
+package com.wurmcraft.towers;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Towers extends Game {
+
+    public SpriteBatch batch;
+
+    @Override
+    public void create() {
+
+    }
+}
---
+++ desktop/src/com/wurmcraft/towers/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wurmcraft.towers.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wurmcraft.towers.Towers;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Towers(), config);
+	}
+}
